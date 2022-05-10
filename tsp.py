ba336503d27f1f391ac624bb6b97fffab71508c2
mashiduki <teramotokoshi@teramotosaiwaimurasakinoMacBook-Air.local>
2022-05-11 00:58:51 +0900

    ok

--- tsp.py
+++ tsp.py
@@ -1,5 +1,5 @@
-from re import T
-from dwave.system import DWaveSampler,EmbeddingComposite
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
 import numpy as np
 from pyqubo import Array,Constraint,Placeholder
 
@@ -9,9 +9,9 @@ Q=np.array([[1000,20,20,50,40],
             [20,10,1000,30,20],
             [50,30,20,1000,10],
             [40,20,20,10,1000]])
-x=Array.create('x',shape=(N,N),vartype='BINARY')
-cost=0
+x=Array.create('x',shape=(N, N),vartype='BINARY')
 #目的関数
+cost=0
 for i in range(N):
     for j in range(N):
         cost+=Q[i][j]*x[i][j]
@@ -24,10 +24,13 @@ constr2=0
 for i in range(N):
     constr2+=(np.sum(x.T[i])-1)**2
 
-cost_func=cost+Placeholder('lambda')*Constraint(constr1,label='constr1')+Placeholder('lambda')*Constraint(constr1,label='constr2')
+cost_func=cost+Placeholder('lambda')*Constraint(constr1,label='constr1')+Placeholder('lambda')*Constraint(constr2,label='constr2')
 model=cost_func.compile()
 feed_dict={'lambda':100.0}
 qubo,offset=model.to_qubo(feed_dict=feed_dict)
-response=DWaveSampler().sample_qubo(qubo,num_reads=100)
-for s,e,o in response.data(['sample','energy','num_occurrences']):
-    print(s,e,o)
+sampleset=EmbeddingComposite(DWaveSampler()).sample_qubo(qubo,num_reads=100)
+print(sampleset.record)
+decoded_samples=model.decode_sampleset(sampleset=sampleset,feed_dict=feed_dict)
+for sample in decoded_samples:
+    print(sample.constraints(only_broken=True))
+print(sampleset.record[0][0].reshape(N, N))
